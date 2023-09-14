00dc4f6aaffd6eca64a82a0d6b2322ab4148fb16
Fierce Bird <92889263+ChocolateCupcake25@users.noreply.github.com>
2023-09-14 16:37:18 +0530

    Add files via upload

---
+++ P-239.py
@@ -0,0 +1,37 @@
+import hashlib
+import time
+import json
+
+def block(proof,previous_hash=None):
+	transaction={
+	"Sender" : "Heena"
+	"Receiver" : "Ziyah"
+	"amount" : '5 ETH'
+	}
+	data={
+	'block_height' : 12913586,
+	'timestamp': time(),
+	'transactions' : transaction,
+	'Block Reward' :2.557309540563,
+	'Uncle Reward' :6598,
+	'Difficulty' :7317161775076869,
+	'Total Difficulty' :0.1,
+	'Size' :0.1,
+	'Gas Used' :0.1,
+	'Gas Limit' :0.1,
+	'proof' : proof,
+	'previous_hash': previous_hash
+	}
+	chain.append(data)
+	print("Block Info: ", data)
+	jobject = json.dump(data)
+	eobject = jobject.encode()
+
+	raw_hash = hashlib.sha512(eobject)
+	hex_hash = raw_hash.hexdigest()
+	print("Hash of Block: " ,hex_hash)
+
+block(previous_hash="No previous Hash. Since this is the first block.",proof=000)
+
+
+
